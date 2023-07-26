b4a3efd6e580d9bda608c713b11f90625fcd7438
kyh018 <109337296+kyh018@users.noreply.github.com>
2023-07-26 05:57:22 +0000

    0726

--- animation.js
+++
@@ -1,6 +0,0 @@
-const toTopEl = document.querySelector('#to-top'); // 먼저 html 파일에 to-top이라는 id로 생성해둔 버튼요소를 가져온다.
-toTopEl.addEventListener('click', function(){ // 해당 버튼을 클릭했을 경우 동작시킬 함수를 작성한다.
-	gsap.to(window, 0.7, {	// gsap의 사용방법
-    	scrollTo: 0		// 이 옵션을 사용하기 위해 ScrollToPlugin을 추가한 것. 
-    });
-});
---
+++ graph.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const chartDataY = [14.9172, 9.8899, 9.4383, 10.9818, 11.9028, 16.5718, 12.9816, 13.1530]; // y값 꿀벌
+    const chartDataX = [0, 57, 114, 171, 228, 285, 342, 399]; // x값 년도
+    const chartDataZ = [12.1, 12.6, 12.8, 13.1, 13.4, 12.8, 12.8, 13.3]; //y2값 기온
+    let chartData = '';
+    let chartDataz='';
+  
+    chartDataX.forEach((x, i) => {
+      chartData += `${x},${chartDataY[i] * 20-150} `;
+    });
+    chartDataX.forEach((x, i) => {
+      chartDataz += `${x},${chartDataZ[i] * 90 -1050} `;
+    });
+  
+    chartData = chartData.trim();
+    chartDataz = chartDataz.trim();
+    const polylineElement = document.querySelector('.polyline1');
+    const polylineElement2 = document.querySelector('.polyline2');
+    if (polylineElement !=null) {
+      polylineElement.setAttribute('points', chartData);
+      polylineElement2.setAttribute('points', chartDataz);
+      console.log("!!!!")
+    } else {
+      console.error('polyline element not found.');
+    }
+    
+  });
